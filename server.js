f705d5d47e82c6fe923706f32cec5edc3c1f663a
nchigumbura <nchigumbura@byupathway.edu>
2026-01-22 15:34:44 +0200

    commit

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // index route
-app.get("/", baseController.buildHome)
+New: app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
@@ -46,9 +46,10 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
